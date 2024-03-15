2eb8c8e006f526023d088802bb637f163f8af10e
Nilay Singh <snilay011@gmail.com>
2024-03-15 17:21:10 +0530

    Get All Category & Delete Categories Functionality Completed

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -27,3 +27,49 @@ const addCategory = async (req,res) => {
       })
     }
 }
+
+const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+
+    return res.status(200).json({
+      success: true,
+      message: "Categories retrieved successfully",
+      categories,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+
+    const category = await Category.findByIdAndDelete(categoryId);
+
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Category deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = {addCategory,getAllCategories,deleteCategory};
