36f73de085795e5f88d161c818283b04acfa14af
Nilay Singh <snilay011@gmail.com>
2024-03-15 17:27:55 +0530

    Backend Setup complete

--- server/index.js
+++ server/index.js
@@ -1,24 +1,36 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./db');
+const dotenv = require('dotenv');
+const db = require('./db/index');
 
 dotenv.config();
 
-const app = express();
-connectDB();
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
+const blogRouter = require('./routes/blog');
+const categoryRouter = require('./routes/category');
+
+db();
 const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.status(200).json({
-        status:"success",
+        success:true,
         message:"Hello World"
     });
 });
 
+app.use("/uploads",express.static("uploads"));
+
+app.use("/api/auth",authRouter);
+app.use("/api/blog",blogRouter);
+app.use("/api/user",userRouter);
+app.use("/api/category",categoryRouter);
+
 app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on PORT ${PORT}`);
 });
