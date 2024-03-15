5b84cdfdd03775bbeb8f82c101c536fbc300ed16
Nilay Singh <snilay011@gmail.com>
2024-03-15 08:07:17 +0530

    Login User functionality Added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -179,3 +179,58 @@ const generateOtp = async(req,res) => {
         })
     }
 }
+
+const loginUser = async (req,res) => {
+    try{
+        const {email,password} = req.body;
+ 
+        if(!email || !password){
+           return res.status(400).json({
+             success:false,
+             message:"Please Enter all fields"
+           });
+        }
+ 
+        const user = await User.findOne({email});
+        if(!user){
+           return res.status(400).json({
+             success:false,
+             message:"User not found"
+           });         
+        }
+        if(user.role !== "user"){
+            return res.status(200).json({
+                success:false,
+                message:"Admin cannot login here"
+            });
+        }
+ 
+        const checkPassword = await bcrypt.compare(password,user.password);
+ 
+        if(!checkPassword){
+           return res.status(400).json({
+             success:false,
+             message:"Invalid Credentials"
+           });           
+        }
+ 
+        const token = jwt.sign({id:user._id,role:user.role},process.env.JWT_SECRET,{expiresIn:864000});
+ 
+        res.status(200).json({
+          success:true,
+          message:"User logged in successfully",
+          token,
+          user:{
+             id:user._id,
+             name:user.name,
+             email:user.email,
+          }
+        });
+ 
+     }catch(error){
+         return res.status(500).json({
+             success:false,
+             message:error,
+         }); 
+     }  
+}
